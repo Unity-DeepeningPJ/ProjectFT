41e354bc93bd98c41f64b8690960f7440a44c298
Bosung <gobosung2000@gmail.com>
2025-03-27 17:29:52 +0900

    [Feat] 슬롯 생성 + UI로 그려주기 성공

--- Assets/_Scripts/Inventory/InventoryManager.cs
+++ Assets/_Scripts/Inventory/InventoryManager.cs
@@ -9,7 +9,6 @@ public class InventoryManager : MonoBehaviour
     // 인벤토리 보유 아이템list 
     // 인벤토리가 변했을때 다시 초기화 해주는 이벤트도 만들어야됨 
     public int MaxSlots = 16;
-    public List<UiSlot> slots;
 
 
     public Dictionary<int, ItemData> inventoryItem;
@@ -22,13 +21,9 @@ public class InventoryManager : MonoBehaviour
     {
         //아이템 데이터
         inventoryItem = new Dictionary<int, ItemData>();
-        slots = new List<UiSlot>();
+        
 
-        //슬롯 생성만  > UI도 그려주기 
-        for (int i = 0; i < MaxSlots; i++)
-        {
-            slots.Add(new UiSlot());
-        }
+        
 
     }
 }
--- Assets/_Scripts/Inventory/UiInventory.cs
+++ Assets/_Scripts/Inventory/UiInventory.cs
@@ -4,19 +4,37 @@ using UnityEngine;
 
 public class UiInventory : MonoBehaviour
 {
-    [SerializeField] private UiSlot uiSlot;
     
+    public List<UiSlot> slots;
+    [SerializeField] private UiSlot uiSlotPrefab;
+    [SerializeField] private Transform slotsparent;
+
 
     private void Awake()
     {
         //슬롯 데이터 
-        
-        Init();
+        slots = new List<UiSlot>();
+
+
+
     }
 
+    private void Start()
+    {
+        Init();
+    }
     private void Init()
     {
-        //GameManager.Instance.inv
+        
+
+        //슬롯 생성만  > UI도 그려주기 
+        for (int i = 0; i < GameManager.Instance.inventoryManager.MaxSlots; i++)
+        {
+            UiSlot slotobj =Instantiate(uiSlotPrefab, slotsparent);
+            slots.Add(slotobj);
+        }
+
+        // 생성해주기? 
     }
 
     // 슬롯을 더해주는 기능 
