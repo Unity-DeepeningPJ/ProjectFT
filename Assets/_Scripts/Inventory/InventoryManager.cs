2a5bc36e7403d310f2fab51924c6de0845f73fcb
Bosung <gobosung2000@gmail.com>
2025-03-27 16:46:52 +0900

    [Feat] inventory manager제작중

---
+++ Assets/_Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    //인벤토리 슬롯 list 
+    // 인벤토리 보유 아이템list 
+    // 인벤토리가 변했을때 다시 초기화 해주는 이벤트도 만들어야됨 
+
+
+    public List<UiSlot> slots;
+
+}
--- Assets/_Scripts/Inventory/UiInventory.cs
+++ Assets/_Scripts/Inventory/UiInventory.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InventoryManager : MonoBehaviour
+public class UiInventory : MonoBehaviour
 {
+
+    public int MaxSlots = 16;
     // Start is called before the first frame update
     void Start()
     {
