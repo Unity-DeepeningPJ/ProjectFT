2e1808fffa13516003ffdec1b963426056846fef
geroer <110544587+geroer@users.noreply.github.com>
2025-03-27 16:48:07 +0900

    [Init] : PlayerState 추가

---
+++ Assets/_ScriptableObject/PlayerData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Player", menuName = "Character/Player")]
+public class PlayerData : ScriptableObject
+{
+    public string playerName;
+    public int basePower;
+    public int baseDefense;
+    public int baseHealth;
+    public float baseSpeed;
+    public float baseJumpPower;
+    public float baseCriticalChance;
+
+    public int startLevel;
+    public int startExp;
+    public int expToNextLevel;
+}
---
+++ Assets/_Scripts/Player/PlayerState.cs
@@ -0,0 +1,59 @@
+﻿public class PlayerState : BaseState
+{
+    public string Name { get; private set; }
+    public int Level { get; private set; }
+    public int CurrentExp {  get; private set; }
+    public int ExpToNextLevel { get; private set; }
+
+    public float CriticalChance { get; private set; }
+
+    private int _equipPower;
+    private int _equipDefense;
+    private int _equipHealth;
+    private float _equipCriticalChance;
+
+    public int TotalPower => Power + _equipPower;
+    public int TotalDefense => Defense + _equipDefense;
+    public int TotalHealth => health + _equipHealth;
+    public float TotalCriticalChance => CriticalChance + _equipCriticalChance;
+
+    public PlayerState(PlayerData data) : base(data.basePower, data.baseDefense, data.baseHealth, data.baseSpeed, data.baseJumpPower)
+    {
+        Name = data.playerName;
+        Level = data.startLevel;
+        CurrentExp = data.startExp;
+        ExpToNextLevel = data.expToNextLevel;
+        CriticalChance = data.baseCriticalChance;
+
+        _equipPower = 0;
+        _equipDefense = 0;
+        _equipHealth = 0;
+        _equipCriticalChance = 0;
+    }
+
+    public void AddExp(int amount)
+    {
+        CurrentExp += amount;
+        if (CurrentExp >= ExpToNextLevel)
+        {
+            LevelUp();
+        }
+    }
+
+    public void LevelUp()
+    {
+        Level++;
+        CurrentExp = 0;
+        ExpToNextLevel = (int)(ExpToNextLevel * 1.2f);
+    }
+
+    public void Equip()
+    {
+
+    }
+
+    public void UnEquip()
+    {
+
+    }
+}
