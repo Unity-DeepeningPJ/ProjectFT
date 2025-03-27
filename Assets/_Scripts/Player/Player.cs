ed55ed3818eafc25127b99d87e4d4049a1c1d9cf
geroer <110544587+geroer@users.noreply.github.com>
2025-03-27 17:15:18 +0900

    [Init] : Player 이동 관련 스크립트 추가

---
+++ Assets/_Scripts/Player/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public PlayerController Controller {  get; private set; }
+
+    private void Awake()
+    {
+        Controller = GetComponent<PlayerController>();
+    }
+}
---
+++ Assets/_Scripts/Player/StateMachine/StateMachine.cs
@@ -0,0 +1,35 @@
+﻿public interface IState
+{
+    public void Enter();
+    public void Exit();
+    public void HandleInput();
+    public void Update();
+    public void PhysicsUpdate();
+}
+
+public abstract class StateMachine
+{
+    protected IState currentState;
+
+    public void ChangeStats(IState state)
+    {
+        currentState?.Exit();
+        currentState = state;
+        currentState?.Enter();
+    }
+
+    public void HandleInput()
+    {
+        currentState?.HandleInput();
+    }
+
+    public void Update()
+    {
+        currentState?.Update();
+    }
+
+    public void PhysicsUpdate()
+    {
+        currentState?.PhysicsUpdate();
+    }
+}
