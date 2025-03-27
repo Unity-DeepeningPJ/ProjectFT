1f14a5c08f48940b84c900a1c41befd2ccbd55c8
alsoox <alsoox173@gmail.com>
2025-03-27 16:47:07 +0900

    [feat] Item 데이터 추가

--- Assets/_ScriptableObject/ItemData.cs
+++ Assets/_ScriptableObject/ItemData.cs
@@ -1,12 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
-
-public enum ItemType
-{
-    Equipment,
-    consumable 
-}
 
 public enum EquipType
 {
@@ -16,14 +11,25 @@ public enum EquipType
     Glove
 }
 
+[SerializeField]
+public class EquipItem
+{
+    public EquipType equipType;
+    public float value;
+}
+
+
+
 [CreateAssetMenu(fileName = "Item", menuName = "New Item")]
 public class ItemData : ScriptableObject
 {
     [Header("Info")]
-    public ItemType type;
+    public int id;
+    public EquipType type;
     public string itemName;
     public string description;
+    public int value;
+
     public Sprite Icon;
     public GameObject itemObj;
-    
 }
