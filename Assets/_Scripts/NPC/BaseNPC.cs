9b439eb1e3e9b34bbe0e61dc9a5963503a6c5e74
Anvil925 <anvil_925@naver.com>
2025-03-28 20:50:13 +0900

    [Init]: NPC관련 CS Files

--- Assets/_Scripts/NPC/BaseNPC.cs
+++ Assets/_Scripts/NPC/BaseNPC.cs
@@ -1,20 +1,22 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Collider2D))]
 public abstract class BaseNPC : MonoBehaviour, INPC
 {
     [Header("기본 NPC 설정")]
-    [SerializeField] protected string npcName;
-    [SerializeField] protected Sprite portrait;
-    [SerializeField] protected bool interactable = true;
+    [SerializeField] protected string npcName; // NPC 이름
+    [SerializeField] protected Sprite portrait; // NPC 초상화
+    [SerializeField] protected bool interactable = true; // 상호작용 가능 여부
     
     [Header("상호작용 설정")]
-    [SerializeField] protected float interactionRadius = 2f;
-    [SerializeField] protected GameObject interactionPrompt;
+    [SerializeField] protected float interactionRadius = 2f; // 상호작용 범위
+    [SerializeField] protected GameObject interactionPrompt; // 상호작용 프롬프트
     [SerializeField] protected KeyCode interactionKey = KeyCode.W;
+    [SerializeField] protected KeyCode cancelKey = KeyCode.S;
     
-    protected bool playerInRange = false;
-    protected PlayerController nearbyPlayer;
+    protected bool playerInRange = false; 
+    protected PlayerController nearbyPlayer; // 근처 플레이어
     
     public string NPCName => npcName;
     public Sprite NPCPortrait => portrait;
@@ -31,11 +33,21 @@ public abstract class BaseNPC : MonoBehaviour, INPC
         // 플레이어가 범위 내에 있고 상호작용 가능하면
         if (playerInRange && interactable)
         {
+            Debug.Log($"{npcName}: 플레이어가 범위 내에 있음, 상호작용 키 대기 중");
+            
             // 상호작용 키를 눌렀을 때
             if (Input.GetKeyDown(interactionKey))
             {
+                Debug.Log($"{npcName}: 상호작용 키 감지됨! 상호작용 시작");
                 Interact(nearbyPlayer);
             }
+        }
+
+        // 상호작용 중 취소키를 눌렀을 때
+        if (Input.GetKeyDown(cancelKey))
+        {
+            Debug.Log($"{npcName}: 상호작용 취소키 감지됨! 상호작용 취소");
+            // 추후 UI매니저를 통해 추가할 예정
         }
     }
     
@@ -44,6 +56,7 @@ public abstract class BaseNPC : MonoBehaviour, INPC
         // 플레이어가 접근했을 때
         if (other.CompareTag("Player"))
         {
+            Debug.Log($"{npcName}: 플레이어 감지됨!");
             playerInRange = true;
             nearbyPlayer = other.GetComponent<PlayerController>();
             
@@ -65,6 +78,10 @@ public abstract class BaseNPC : MonoBehaviour, INPC
             if (interactionPrompt != null)
                 interactionPrompt.SetActive(false);
         }
+
+        // 만약 플레이어가 범위 내에 있고 상호작용 중이라면
+        // 상호작용하여 열려있던 UI를 닫도록 함 추후 UI매니저를 통해 추가할 예정
+
     }
     
     public abstract void Interact(PlayerController player);
--- Assets/_Scripts/NPC/ShopkeeperNPC.cs
+++ Assets/_Scripts/NPC/ShopkeeperNPC.cs
@@ -4,35 +4,23 @@ using UnityEngine;
 public class ShopkeeperNPC : BaseNPC
 {
     [Header("상점 설정")]
-    [SerializeField] private List<ShopItem> shopItems = new List<ShopItem>();
     [SerializeField] private UIStore shopUI;
     
-    [System.Serializable]
-    public class ShopItem
-    {
-        public string itemName;
-        public Sprite icon;
-        public int price;
-        public string description;
-        public ItemData itemPrefab;
-    }
-    
+    private bool isShopOpen = false;
     public override void Interact(PlayerController player)
     {
         // 상점 UI 띄우기
-        if (shopUI != null)
+        if (shopUI != null && !isShopOpen)
         {
-            shopUI.OpenShop(this, player);
-            GameManager.Instance.SetGameState(GameManager.GameState.Paused);
+            shopUI.OpenShop();
+            isShopOpen = true;
         }
         else
         {
-            Debug.LogError($"상점 NPC '{npcName}'에 ShopUI가 할당되지 않았습니다!");
+            shopUI.StoreExit();
+            isShopOpen = false;
+            Debug.LogError($"상점 NPC '{npcName}'에 ShopUI가 할당되지 않았거나 이미 상점이 열려있습니다.");
         }
     }
-    
-    public List<ShopItem> GetShopItems()
-    {
-        return shopItems;
-    }
+    // 
 }
