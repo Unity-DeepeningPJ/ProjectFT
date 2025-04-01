f70bd6d9f7f331dc18b2ab4c972f0d565ea52566
min4937 <pmy1504@naver.com>
2025-04-01 23:19:11 +0900

    [Feat]:디버그 정리

--- Assets/_Scripts/Enemy/Boss.cs
+++ Assets/_Scripts/Enemy/Boss.cs
@@ -1,4 +1,3 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Boss : MeleeEnemy, IDamageable
@@ -21,7 +20,7 @@ public class Boss : MeleeEnemy, IDamageable
     public float offsetRoundShotSideOffset = 2f; // 보스 중심으로부터의 거리
     public float yOffset = 0.5f;
 
-    [Header("Fan Shot")] 
+    [Header("Fan Shot")]
     public int fanShotProjectileCount = 6;   // 부채꼴 발사 투사체 개수
     public float fanShotSpreadAngle = 60f;   // 부채꼴 총 확산 각도
     public float fanShotJumpPower = 5f;      // 점프 힘
@@ -33,14 +32,14 @@ public class Boss : MeleeEnemy, IDamageable
 
     [Header("References")]
     public Transform firePoint; // 투사체 발사 위치
-    
+
     private float nextSpecialAttackTime;
     private bool isSpecialAttacking = false; // 특수 공격 중인지 여부
 
     public enum SpecialAttackType
     {
         DashAttack,
-        JumpRoundShot,
+        RoundShot,
         FanShot
     }
 
@@ -88,13 +87,11 @@ public class Boss : MeleeEnemy, IDamageable
         GameObject player = FindClosestObjectWithLayer(transform.position, playerLayer, maxDetectionDistance);
         if (player != null)
         {
-            playerTransform = player.transform;
-            //Debug.Log("플레이어 찾음");
+            playerTransform = player.transform;            
         }
         else
         {
-            playerTransform = null;
-            //Debug.Log("플레이어 감지 실패");
+            playerTransform = null;            
         }
         if (currentHealth <= 0)
         {
@@ -141,7 +138,6 @@ public class Boss : MeleeEnemy, IDamageable
             StopAttack();
         }
     }
-
     void HandleMovementState()
     {
         if (playerTransform != null)
@@ -149,14 +145,11 @@ public class Boss : MeleeEnemy, IDamageable
             StartAttack();
         }
     }
-
-
-
     void StartAttack()
     {
         if (Time.time >= nextAttackTime && !isSpecialAttacking && playerTransform != null)
         {
-            
+
             isAttacking = true;
             isChasing = true;
 
@@ -169,7 +162,6 @@ public class Boss : MeleeEnemy, IDamageable
             nextAttackTime = Time.time + 1f / attackRate; // 공격 후 쿨타임 적용
         }
     }
-
     void Dash()
     {
         if (isChasing)
@@ -183,7 +175,6 @@ public class Boss : MeleeEnemy, IDamageable
             }
         }
     }
-
     void StopAttack()
     {
         isAttacking = false;
@@ -202,10 +193,10 @@ public class Boss : MeleeEnemy, IDamageable
             case SpecialAttackType.DashAttack:
                 StartCoroutine(DashAttack());
                 break;
-            case SpecialAttackType.JumpRoundShot:
-                StartCoroutine(JumpRoundShot());
+            case SpecialAttackType.RoundShot:
+                StartCoroutine(RoundShot());
                 break;
-            case SpecialAttackType.FanShot: 
+            case SpecialAttackType.FanShot:
                 StartCoroutine(FanShot());
                 break;
         }
@@ -238,13 +229,10 @@ public class Boss : MeleeEnemy, IDamageable
     }
 
     System.Collections.IEnumerator SpreadShot()
-    {
-        // Debug.Log("SpreadShot");
-        Vector2 directionToPlayer = (playerTransform.position - transform.position).normalized;
-        // Debug.Log("directionToPlayer: " + directionToPlayer); // 방향 벡터 확인
-
+    {        
+        Vector2 directionToPlayer = (playerTransform.position - transform.position).normalized;        
         animi.OnAttack(true);
-        
+
         for (int i = 0; i < spreadShotProjectileCount; i++)
         {
             GameObject projectile = ObjectPool.Instance.GetPooledObject();
@@ -271,10 +259,8 @@ public class Boss : MeleeEnemy, IDamageable
 
                 if (projectileRb != null)
                 {
-                    projectileRb.velocity = directionToPlayer * projectileSpeed; // 투사체 속도 설정
-                    // Debug.Log("Projectile " + i + " velocity: " + projectileRb.velocity); // 속도 확인
-                    projectile.SetActive(true);
-                    // Debug.Log("Projectile " + i + " activa   ted"); // 활성화 확인
+                    projectileRb.velocity = directionToPlayer * projectileSpeed; // 투사체 속도 설정                    
+                    projectile.SetActive(true);                    
                 }
                 else
                     Debug.LogError("투사체에 rigidbody가 없습니다");
@@ -288,13 +274,11 @@ public class Boss : MeleeEnemy, IDamageable
         isSpecialAttacking = false;
     }
 
-    System.Collections.IEnumerator JumpRoundShot()
-    {
-        // Debug.Log("JumpRoundShot");
+    System.Collections.IEnumerator RoundShot()
+    {        
         isSpecialAttacking = true; // 특수 공격 중 상태로 변경
 
-        // 플레이어의 위치를 기준으로 보스의 왼쪽 또는 오른쪽 선택
-        // Debug.Log("playerTransform: " + playerTransform);
+        // 플레이어의 위치를 기준으로 보스의 왼쪽 또는 오른쪽 선택        
         float side = playerTransform.position.x > transform.position.x ? 1f : -1f; // 플레이어가 오른쪽에 있으면 1, 왼쪽에 있으면 -1
 
         // 플레이어 방향과 반대 방향으로 offset 설정
@@ -314,8 +298,7 @@ public class Boss : MeleeEnemy, IDamageable
             if (projectile != null)
             {
                 // 투사체 생성 위치 조정
-                Vector3 spawnPosition = firePoint.position + offset + Vector3.up * yOffset;
-                // Debug.Log("spawnPosition: " + spawnPosition);
+                Vector3 spawnPosition = firePoint.position + offset + Vector3.up * yOffset;                
                 projectile.transform.position = spawnPosition;
                 projectile.transform.rotation = Quaternion.identity;
 
@@ -331,7 +314,6 @@ public class Boss : MeleeEnemy, IDamageable
                         bulletScript.SetDirection(direction);
                     }
 
-                    // Debug.Log("projectileRb.velocity: " + projectileRb.velocity);
                     projectile.SetActive(true);
                 }
                 else
@@ -345,25 +327,21 @@ public class Boss : MeleeEnemy, IDamageable
         isSpecialAttacking = false;
     }
     System.Collections.IEnumerator FanShot()
-    {
-        Debug.Log("Executing FanShot");
-        isSpecialAttacking = true; 
-        
+    {        
+        isSpecialAttacking = true;
 
         if (playerTransform == null)
-        {
-            Debug.LogWarning("FanShot: PlayerTransform is null. Aborting attack.");
+        {            
             isSpecialAttacking = false;
             yield break; // 플레이어가 없으면 코루틴 종료
         }
-        
-        
+
         rb.AddForce(Vector2.up * fanShotJumpPower, ForceMode2D.Impulse); // 위쪽으로 힘을 가해서 점프
         yield return new WaitForSeconds(0.3f); // 점프 후 0.3초 정도 대기         
 
-                
-        Vector2 centerDirection = (playerTransform.position - firePoint.position).normalized;               
-        float startAngle = -fanShotSpreadAngle / 2f;       
+
+        Vector2 centerDirection = (playerTransform.position - firePoint.position).normalized;
+        float startAngle = -fanShotSpreadAngle / 2f;
         float angleStep = (fanShotProjectileCount > 1) ? fanShotSpreadAngle / (fanShotProjectileCount - 1) : 0f;
 
         // 4. 투사체 발사
@@ -382,29 +360,29 @@ public class Boss : MeleeEnemy, IDamageable
             {
                 projectile.transform.position = firePoint.position;
                 // 선택 사항: 투사체가 발사 방향을 바라보도록 회전
-                projectile.transform.rotation = Quaternion.LookRotation(Vector3.forward, fireDirection); 
+                projectile.transform.rotation = Quaternion.LookRotation(Vector3.forward, fireDirection);
 
                 Rigidbody2D projectileRb = projectile.GetComponent<Rigidbody2D>();
                 Bullet bulletScript = projectile.GetComponent<Bullet>(); // Bullet 스크립트 가져오기
 
                 if (projectileRb != null && bulletScript != null)
-                {                    
-                    bulletScript.SetDirection(fireDirection);                    
+                {
+                    bulletScript.SetDirection(fireDirection);
                     projectileRb.velocity = fireDirection * projectileSpeed;// Rigidbody 속도 설정
                     projectile.SetActive(true);
-                    
+
                 }
                 else
-                {                    
+                {
                     projectile.SetActive(false);
                 }
-            }           
-            
+            }
+
         }
-                
+
         yield return new WaitForSeconds(0.1f); // 아주 짧은 후딜레이        
         isSpecialAttacking = false; // 특수 공격 종료
-        
+
     }
     public void TakePhysicalDamage(int damage)
     {
@@ -421,7 +399,7 @@ public class Boss : MeleeEnemy, IDamageable
         {
             Vector3 spawnPos = new Vector3(
                 Random.Range(transform.position.x - 4f, transform.position.x + 4f),
-                transform.position.y +3,
+                transform.position.y + 3,
                 transform.position.z
             );
             Instantiate(coinPrefab, spawnPos, Quaternion.identity);
@@ -432,7 +410,7 @@ public class Boss : MeleeEnemy, IDamageable
         GameObject npcObj = GameObject.FindGameObjectWithTag("NPC");
         ShopkeeperNPC npc = npcObj.GetComponent<ShopkeeperNPC>();
         npc.MoveNPC();
-        
+
 
         GameObject player = GameObject.FindGameObjectWithTag("Player");
 
@@ -449,37 +427,37 @@ public class Boss : MeleeEnemy, IDamageable
         }
     }
 
-        GameObject FindClosestObjectWithLayer(Vector3 position, LayerMask playerLayer, float maxDistance)
+    GameObject FindClosestObjectWithLayer(Vector3 position, LayerMask playerLayer, float maxDistance)
+    {
+        GameObject closest = null;
+        float closestDistanceSqr = Mathf.Infinity;
+        Vector3 pos = position;
+
+        // 지정된 레이어 마스크에 해당하는 모든 GameObject 찾기
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(position, maxDistance, playerLayer);
+
+        foreach (Collider2D collider in colliders)
         {
-            GameObject closest = null;
-            float closestDistanceSqr = Mathf.Infinity;
-            Vector3 pos = position;
+            GameObject go = collider.gameObject;
+            Vector3 diff = go.transform.position - pos;
+            float distanceSqr = diff.sqrMagnitude;
 
-            // 지정된 레이어 마스크에 해당하는 모든 GameObject 찾기
-            Collider2D[] colliders = Physics2D.OverlapCircleAll(position, maxDistance, playerLayer);
-
-            foreach (Collider2D collider in colliders)
+            if (distanceSqr < closestDistanceSqr)
             {
-                GameObject go = collider.gameObject;
-                Vector3 diff = go.transform.position - pos;
-                float distanceSqr = diff.sqrMagnitude;
-
-                if (distanceSqr < closestDistanceSqr)
-                {
-                    closest = go;
-                    closestDistanceSqr = distanceSqr;
-                }
+                closest = go;
+                closestDistanceSqr = distanceSqr;
             }
-
-            // 만약 플레이어가 maxDistance 내에 없다면 closest는 null을 반환
-            if (closestDistanceSqr == Mathf.Infinity) // 거리가 초기값과 같다면, 플레이어를 찾지 못했다는 의미
-            {
-                return null; // 플레이어를 찾지 못했으면 null 반환
-            }
-
-
-            return closest;
         }
 
-    
+        // 만약 플레이어가 maxDistance 내에 없다면 closest는 null을 반환
+        if (closestDistanceSqr == Mathf.Infinity) // 거리가 초기값과 같다면, 플레이어를 찾지 못했다는 의미
+        {
+            return null; // 플레이어를 찾지 못했으면 null 반환
+        }
+
+
+        return closest;
+    }
+
+
 }
